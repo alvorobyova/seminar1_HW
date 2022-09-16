e74eedb0f52044fffaf3190d4b000df4d3c6642c
Alena <alena.vorobyevaa@gmail.com>
2022-09-16 10:07:48 +0200

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA + " большее число");
+}
+else if(numberA < numberB)
+{
+    Console.WriteLine(numberB + " большее число");
+}
+
+else if(numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+
+}
