25fb1875fec1e1f7139b2fb584aa4b4656bc4cbb
Alena <alena.vorobyevaa@gmail.com>
2022-09-16 10:17:12 +0200

    Task6 added

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("В этой программе будем определять четность/ нечетность чисел.");
+Console.WriteLine("Поехали!");
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Введенное вами число является четным");
+
+}
+
+else 
+{
+    Console.WriteLine("Вы ввели нечетное число");
+}
