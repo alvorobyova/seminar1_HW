5f87ea371a374b095eed5afbea52f15012077877
Alena <alena.vorobyevaa@gmail.com>
2022-09-16 10:56:25 +0200

    Task8 added

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("В этой программе будем определять четные числа в заданном диапазоне.");
+Console.WriteLine("Поехали!");
+
+Console.WriteLine("Введите любое число больше 1");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Отлично! Определим все четные числа от 1 до " + number);
+Console.WriteLine("Четные числа в заданном диапазоне:");
+
+for(int start = 1; start <= number; start++)
+if(start % 2 == 0) 
+{
+Console.Write(start + ", ");
+}
