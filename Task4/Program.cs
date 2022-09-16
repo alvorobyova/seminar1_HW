b74005dd07ef5223dc4b8fe58ef207fd4c4fd8f4
Alena <alena.vorobyevaa@gmail.com>
2022-09-16 10:12:00 +0200

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите 1-е число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2-е число");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 3-е число");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(numberA > max) 
+{
+    max = numberA;
+}
+else if(numberB > max)
+{
+    max = numberB;
+}
+
+else if(numberC > max)
+
+{
+    max = numberC;
+}
+Console.WriteLine(max + " наибольшее из 3 чисел");
